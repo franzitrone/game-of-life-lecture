574e47d7ca2b40f24e61e5ca74e7a6064ea8428c
Timo Zuccarello <timo@zuccarello.eu>
2024-12-17 17:10:57 +0100

    Add exercise base states.

---
+++ app/src/main/java/de/uulm/sp/swt/statepattern/AliveState.java
@@ -0,0 +1,12 @@
+package de.uulm.sp.swt.statepattern;
+
+import java.util.List;
+
+public class AliveState implements State {
+
+  @Override
+  public State operation(List<State> neighbours) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'operation'");
+  }
+}
---
+++ app/src/main/java/de/uulm/sp/swt/statepattern/DeadState.java
@@ -0,0 +1,12 @@
+package de.uulm.sp.swt.statepattern;
+
+import java.util.List;
+
+public class DeadState implements State {
+
+  @Override
+  public State operation(List<State> neighbours) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'operation'");
+  }
+}
