703325a07536c22a1de6754ed48107e78e6a8a6a
Timo Zuccarello <timo@zuccarello.eu>
2024-12-17 17:10:35 +0100

    Add comment to operation method in State.

--- app/src/main/java/de/uulm/sp/swt/statepattern/State.java
+++ app/src/main/java/de/uulm/sp/swt/statepattern/State.java
@@ -3,5 +3,11 @@ package de.uulm.sp.swt.statepattern;
 import java.util.List;
 
 public interface State {
+  /**
+   * Returns the new state of a cell.
+   * 
+   * @param neighbours The list of neighbouring cell states.
+   * @return the new state of the cell according to the game rules.
+   */
   public State operation(List<State> neighbours);
 }
